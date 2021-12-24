103b96e76adb731f1ab0ef0a990f49edb84c8eb9
paoladonolato <paoladono98@gmail.com>
2021-12-24 18:01:03 +0100

    punto 3.5

--- excersise3_5.r
+++ excersise3_5.r
@@ -23,8 +23,20 @@ t_test <- function(data){
   n_rows <- dim(data)[1]
   t_test_result <- c()
   for (i in (1:n_rows)){
-    t_test_result[i] <- t.test(group1[i,], group2[i,], alternative = "two.sided", mu = mean_diff[i], conf.level = alpha)
+    t_test_result[i] <- t.test(group1[i,], group2[i,], alternative = "two.sided", mu = mean_diff[i], conf.level = alpha)$p.value
   }
   return (t_test_result)
 }
 
+wilcoxon_test <- function(data){
+  group1 <- data[, c(15:19)]
+  group2 <- data[, c(1:14)]
+  alpha <- 0.05
+  n_rows <- dim(data)[1]
+  wilcoxon_test_result <- c()
+  for (i in (1:n_rows)){
+    wilcoxon_test_result[i] <- wilcox.test(group1[i,], group2[i,], alternative = "two.sided", mu = 0, paired = FALSE)$p.value
+    
+  }
+  return (wilcoxon_test_result)
+}
